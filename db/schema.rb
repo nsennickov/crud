df6a09749eb10f7ed17c98585b52de9ec59c5b94
nsennickov <nikitasennikov.off@gmail.com>
2021-01-11 16:48:06 +0200

    Rename a db table-name

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_109_200_405) do
-  create_table 'items', force: :cascade do |t|
-    t.string 'todo'
-    t.boolean 'isDone'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+ActiveRecord::Schema.define(version: 2021_01_11_143615) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "todo"
+    t.boolean "done"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
 end
