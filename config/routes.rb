bb9507958988f1afa386680f05a3a87d8025c67d
nsennickov <nikitasennikov.off@gmail.com>
2021-01-11 18:45:10 +0200

    Rename tablecell + changes from Kill Real comments)

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,5 @@
 Rails.application.routes.draw do
   root 'items#index'
 
-  # get "item/new"
-  # post 'item/new'
-  # post 'item/:id' => 'item#edit'
-  # post 'item/:id/edit' => 'item#update'
-  # post 'item/destroy/:id' => 'item#destroy'
-
   resources :items
 end
