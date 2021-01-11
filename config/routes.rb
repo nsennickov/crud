51e6267ced62a599c841ad5c2a721ca1d02fa01d
nsennickov <nikitasennikov.off@gmail.com>
2021-01-11 12:36:28 +0200

    Rubocop is here

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'items#index'
-  
+
   # get "item/new"
   # post 'item/new'
   # post 'item/:id' => 'item#edit'
   # post 'item/:id/edit' => 'item#update'
   # post 'item/destroy/:id' => 'item#destroy'
-  
+
   resources :items
 end
--- db/migrate/20210109200405_create_items.rb
+++ db/migrate/20210109200405_create_items.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
-	create_table :items do |t|
-		t.string :todo
-		t.boolean :isDone
+    create_table :items do |t|
+      t.string :todo
+      t.boolean :isDone
 
-		t.timestamps
-	end
+      t.timestamps
+    end
   end
 end
