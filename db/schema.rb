808d36c8b2fa476e240d2f02f367c540baad3d15
1 <a1@1s-MacBook-Pro-2.local>
2021-01-27 15:12:01 +0200

    Add i18n gem

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_115_160_622) do
+ActiveRecord::Schema.define(version: 2021_01_15_160622) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
   create_table 'items', force: :cascade do |t|
-    t.string 'todo'
+    t.text 'todo'
     t.boolean 'done'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
